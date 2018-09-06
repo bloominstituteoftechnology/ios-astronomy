541aca69f2f93cb3bdda1da30f8d99d8d17b48c1
Simon Elhoej <simonelhoej@gmail.com>
2018-09-07 00:14:40 +0200

    initial commit

--- Astronomy/View Controllers/PhotosCollectionViewController.swift
+++ Astronomy/View Controllers/PhotosCollectionViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class PhotosCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
+    let cache = Cache<Int, Data>()
+    var fetchOperationDictionary: [Int: FetchPhotoOperation] = [:]
+    let photoFetchQueue = OperationQueue()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -64,9 +68,56 @@ class PhotosCollectionViewController: UIViewController, UICollectionViewDataSour
     
     private func loadImage(forCell cell: ImageCollectionViewCell, forItemAt indexPath: IndexPath) {
         
-        // let photoReference = photoReferences[indexPath.item]
+        let photoReference = photoReferences[indexPath.item]
+        let id = photoReference.id
+        fetchOperationDictionary[id] = FetchPhotoOperation(photoReference: photoReference)
+        
+        if cache.imageIsCached(id: id)
+        {
+            cell.imageView.image = UIImage(data: cache.value(for: id)!)
+        }
+        else
+        {
+            let fetchOperation = FetchPhotoOperation(photoReference: photoReference)
+            let cacheOperation = BlockOperation {
+                
+                if let data = fetchOperation.imageData
+                {
+                    self.cache.addToCache(value: data, key: id)
+                }
+            }
+            
+            let completionOperation = BlockOperation {
+                
+                defer { self.fetchOperationDictionary.removeValue(forKey: id) }
+                if let currentIndexPath = self.collectionView.indexPath(for: cell), currentIndexPath != indexPath
+                {
+                    return
+                }
+                
+                if let data = fetchOperation.imageData
+                {
+                    cell.imageView.image = UIImage(data: data)
+                }
+            }
+            
+            cacheOperation.addDependency(fetchOperation)
+            completionOperation.addDependency(fetchOperation)
+            
+            photoFetchQueue.addOperation(fetchOperation)
+            photoFetchQueue.addOperation(cacheOperation)
+            OperationQueue.main.addOperation(completionOperation)
+            
+            fetchOperationDictionary[id] = fetchOperation
+        }
         
         // TODO: Implement image loading here
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath)
+    {
+        let photoReference = photoReferences[indexPath.item]
+        fetchOperationDictionary[photoReference.id]?.cancel()
     }
     
     // Properties
