49f8e95a4781667daa72b815efdaec6c799b6789
stdemicheli <stdemicheli@gmail.com>
2018-09-07 00:39:53 +0200

    Added PhotoFetchOperation enabling cancellation of a fetch operation.

---
+++ Astronomy/Networking/PhotoFetchOperation.swift
@@ -0,0 +1,45 @@
+//
+//  PhotoFetchOperation.swift
+//  Astronomy
+//
+//  Created by De MicheliStefano on 07.09.18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class PhotoFetchOperation: ConcurrentOperation {
+    
+    var photoReference: MarsPhotoReference
+    var imageData: Data?
+    private var dataTask: URLSessionDataTask?
+    
+    init(for photoReference: MarsPhotoReference) {
+        self.photoReference = photoReference
+    }
+    
+    override func start() {
+        state = .isExecuting
+        let imageUrl = photoReference.imageURL.usingHTTPS!
+        dataTask = URLSession.shared.dataTask(with: imageUrl) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching images: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("Could load data from API")
+                return
+            }
+            
+            self.imageData = data
+            self.state = .isFinished
+        }
+        dataTask?.resume()
+    }
+    
+    override func cancel() {
+        dataTask?.cancel()
+    }
+    
+}
