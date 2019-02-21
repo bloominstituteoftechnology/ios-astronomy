362f77549be4bf14e3395b6644826ac44d471d12
Moses Robinson <mosesjrobinson@icloud.com>
2019-02-21 15:24:13 -0800

    cache skeleton created

---
+++ Astronomy/Utilities/Cache.swift
@@ -0,0 +1,26 @@
+//
+//  Cache.swift
+//  Astronomy
+//
+//  Created by Moses Robinson on 2/21/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class Cache<Key: Hashable, Value> {
+    
+    func cache(value: Value, for: Key) {
+        
+    }
+    
+    func value(for: Key) {
+        
+    }
+    
+    
+    
+    
+    
+    private var cachedItems: [Key : Value] = [:]
+}
