773f5dd96046d077039978dbd7c177aa93688de9
Frulwinn <frulwinn@gmail.com>
2019-02-28 14:21:32 -0800

    finished data task in FetchPhotoOperation

--- Astronomy/Operations/FetchPhotoOperation.swift
+++ Astronomy/Operations/FetchPhotoOperation.swift
@@ -18,4 +18,31 @@ class FetchPhotoOperation: ConcurrentOperation {
     init(marsPhotoReference: MarsPhotoReference) {
         self.marsPhotoReference = marsPhotoReference
     }
+    
+    override func start() {
+        state = .isExecuting
+        
+        //Get the URL for the associated image using the imageURL property.
+        guard let url = marsPhotoReference.imageURL.usingHTTPS else { return }
+
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            
+            defer {
+                self.state = .isFinished
+            }
+            
+            if let error = error {
+                NSLog("Error fetching person: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                return
+            }
+            
+            self.imageData = data
+        }
+        dataTask.resume()
+    }
 }
