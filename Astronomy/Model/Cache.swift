3ef4aa664c4b1a1a806b77181f8386610f5ed85a
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-06 16:37:46 -0700

    cache

---
+++ Astronomy/Model/Cache.swift
@@ -0,0 +1,18 @@
+//
+//  Cache.swift
+//  Astronomy
+//
+//  Created by Hector Steven on 6/6/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct Cache<Key: Hashable, Value> {
+	let cache: [Cache] = [:]
+	
+	
+	
+	
+}
