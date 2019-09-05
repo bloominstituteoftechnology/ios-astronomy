1c9dc9c7ac16b4cdd1050ba8837f3ccdb978d44f
bradleyyin <bradleyyin@gmail.com>
2019-09-05 15:53:04 -0700

    construct fetch photo operation

---
+++ Astronomy/Operations/FetchPhotoOperation.swift
@@ -0,0 +1,48 @@
+//
+//  FetchPhotoOperation.swift
+//  Astronomy
+//
+//  Created by Bradley Yin on 9/5/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class FetchPhotoOperation: ConcurrentOperation {
+    var marsPhotoReference: MarsPhotoReference
+    var imageData: Data?
+    
+    var loadImageDataTask : URLSessionDataTask {
+        return URLSession.shared.dataTask(with: marsPhotoReference.imageURL) { (data, response, error) in
+            defer {
+                self.state = .isFinished
+            }
+            if let error = error {
+                NSLog("Error doing load image dataTask: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("no data return on load image")
+                return
+            }
+            
+            self.imageData = data
+            //print("load from internet")
+            
+        }
+    }
+    
+    init(marsPhotoReference: MarsPhotoReference) {
+        self.marsPhotoReference = marsPhotoReference
+    }
+    override func start() {
+        state = .isExecuting
+        loadImageDataTask.resume()
+    }
+    
+    override func cancel() {
+        loadImageDataTask.cancel()
+    }
+    
+}
