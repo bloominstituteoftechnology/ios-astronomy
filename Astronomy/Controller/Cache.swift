cedddbc6f6bb24cbd6b306b7899ae2f516fdf44e
Wilma <kima_92@fastmail.com>
2019-10-31 18:41:38 -0400

    Added Cache class

---
+++ Astronomy/Controller/Cache.swift
@@ -0,0 +1,19 @@
+//
+//  Cache.swift
+//  Astronomy
+//
+//  Created by macbook on 10/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class Cache<Key: Hashable, Value> {
+    
+    private var catchedItems: [Key : Value] = [:]
+    
+    
+    
+    
+    
+}
