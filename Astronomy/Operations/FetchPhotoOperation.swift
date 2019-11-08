ca24560b2d5cb9ffaa84689596bbfa8cb6d3e95f
John Kouris <jdkouris@gmail.com>
2019-11-07 18:03:50 -0800

    Added the properties and overrode the start() and cancel() methods within the FetchPhotoOperation class

--- Astronomy/Operations/FetchPhotoOperation.swift
+++ Astronomy/Operations/FetchPhotoOperation.swift
@@ -11,6 +11,49 @@ import Foundation
 
 class FetchPhotoOperation: ConcurrentOperation {
     
+    var imageData: Data?
+    var dataTask: URLSessionDataTask?
     
+    let id: Int
+    let sol: Int
+    let camera: Camera
+    let earthDate: Date
+    let imageURL: URL
+    
+    init(marsPhotoReference: MarsPhotoReference) {
+        self.id = marsPhotoReference.id
+        self.sol = marsPhotoReference.sol
+        self.camera = marsPhotoReference.camera
+        self.earthDate = marsPhotoReference.earthDate
+        self.imageURL = marsPhotoReference.imageURL
+        super.init()
+    }
+    
+    override func start() {
+        super.start()
+        state = .isExecuting
+        
+        guard let url = imageURL.usingHTTPS else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            defer { self.state = .isFinished }
+            if self.isCancelled { return }
+            
+            if let error = error {
+                print("Error fetching image: \(error)")
+                return
+            }
+            
+            self.imageData = data
+        }
+        
+        task.resume()
+        dataTask = task
+    }
+    
+    override func cancel() {
+        super.cancel()
+        dataTask?.cancel()
+    }
     
 }
