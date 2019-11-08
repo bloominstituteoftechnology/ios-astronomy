a7c78992bb2f5d9c96c0af987f8cd392682ecc4d
John Kouris <jdkouris@gmail.com>
2019-11-07 17:52:26 -0800

    Added the FetchPhotoOperation file

---
+++ Astronomy/Operations/FetchPhotoOperation.swift
@@ -0,0 +1,16 @@
+//
+//  FetchPhotoOperation.swift
+//  Astronomy
+//
+//  Created by John Kouris on 11/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+class FetchPhotoOperation: ConcurrentOperation {
+    
+    
+    
+}
