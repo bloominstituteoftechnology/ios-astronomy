ba8c2554ac5af5ff77e6052daba0a32656fcffd3
Robert Keffury <bobby.keffury@me.com>
2019-11-13 11:17:45 -0800

    Continued Debugging

--- Astronomy/View Controllers/PhotoDetailViewController.swift
+++ Astronomy/View Controllers/PhotoDetailViewController.swift
@@ -32,7 +32,9 @@ class PhotoDetailViewController: UIViewController {
     // MARK: - Private
     
     private func updateViews() {
-        guard let photo1 = photo, isViewLoaded else { return }
+        guard let photo1 = photo,
+            isViewLoaded else {
+                return }
         do {
             let data = try Data(contentsOf: photo1.imageURL)
             imageView.image = UIImage(data: data)
