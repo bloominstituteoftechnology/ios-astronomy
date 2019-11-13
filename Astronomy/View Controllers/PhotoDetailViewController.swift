ca7a7a6c9459ab969802d62715bc8bbdb868896b
Robert Keffury <bobby.keffury@me.com>
2019-11-13 10:31:39 -0800

    Added Detail View Controller.

---
+++ Astronomy/View Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,65 @@
+//
+//  PhotoDetailViewController.swift
+//  Astronomy
+//
+//  Created by Bobby Keffury on 11/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class PhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViews()
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        guard let image = imageView.image else { return }
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAsset(from: image)
+        }, completionHandler: { (success, error) in
+            if let error = error {
+                NSLog("Error saving photo: \(error)")
+                return
+            }
+        })
+    }
+    
+    // MARK: - Private
+    
+    private func updateViews() {
+        guard let photo = photo, isViewLoaded else { return }
+        do {
+            let data = try Data(contentsOf: photo.imageURL)
+            imageView.image = UIImage(data: data)
+            let dateString = dateFormatter.string(from: photo.earthDate)
+            detailLabel.text = "Taken by \(photo.camera.roverId) on \(dateString) (Sol \(photo.sol))"
+            cameraLabel.text = photo.camera.fullName
+        } catch {
+            NSLog("Error setting up views on detail view controller: \(error)")
+        }
+    }
+    
+    // MARK: - Properties
+    
+    var photo: MarsPhotoReference? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    lazy var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .short
+        return df
+    }()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var cameraLabel: UILabel!
+
+}
