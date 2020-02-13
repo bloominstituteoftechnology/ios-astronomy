956002d618c4d2dd2986c21329027eb45d935479
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-02-13 15:14:17 -0800

    Refactored for thread safety

--- Astronomy/Model/cache.swift
+++ Astronomy/Model/cache.swift
@@ -10,12 +10,16 @@ import Foundation
 
 class Cache<Key: Hashable, Value> {
     private var stores = [Key: Value]()
-    
+    private let queue = DispatchQueue(label: "CacheQueue")
     func cache(value: Value, for key: Key) {
-        stores[key] = value
+        queue.sync {
+            self.stores[key] = value
+        }
     }
     
     func value(for key: Key) -> Value? {
-        return stores[key] ?? nil
+        queue.sync {
+            return self.stores[key] ?? nil
+        }
     }
 }
