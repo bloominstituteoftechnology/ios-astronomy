a699beff6882325efdadae8316c7d476f73c92d7
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-02-16 20:55:45 -0800

    Part 1 almost done

---
+++ Astronomy/Operations/FetchPhotoOperation.swift
@@ -0,0 +1,51 @@
+//
+//  FetchPhotoOperation.swift
+//  Astronomy
+//
+//  Created by Jorge Alvarez on 2/16/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class FetchPhotoOperation: ConcurrentOperation {
+    
+    // MARK: - Properties
+    var photoReference: MarsPhotoReference
+    
+    var imageData: Data?
+    
+    var dataTask: URLSessionDataTask?
+    
+    override func start() {
+        state = .isExecuting
+        
+        guard let request = photoReference.imageURL.usingHTTPS else { return }
+        dataTask = URLSession.shared.dataTask(with: request) { data, _, error in
+            defer {
+                self.state = .isFinished
+            }
+            if let error = error {
+                NSLog("Error retrieving photos from server. \(error)")
+                return
+            }
+            guard let data = data else {
+                NSLog("Bad Data")
+                return
+            }
+            self.imageData = data
+            
+        }
+        dataTask?.resume()
+    }
+    
+    override func cancel() {
+        dataTask?.cancel()
+        self.state = .isFinished
+    }
+    
+    init(reference: MarsPhotoReference, session: URLSession = .shared) {
+        self.photoReference = reference
+        super.init()
+    }
+}
--- Astronomy/View Controllers/PhotosCollectionViewController.swift
+++ Astronomy/View Controllers/PhotosCollectionViewController.swift
@@ -6,9 +6,32 @@
 //  Copyright © 2018 Lambda School. All rights reserved.
 //
 
+/*
+
+     Load mission manifest (MarsRover) for a given Rover. (Done for you already)
+     Load photos metadata (MarsPhotoReferences) for a particular solar day. (Done for you)
+     For each photo reference, load the actual image data using the photo reference's imageURL property. (You need to do this.)
+
+ Fill out PhotosCollectionViewController.loadImage(forCell:, forItemAt:). For now, just use URLSession directly.
+
+     Get the MarsPhotoReference instance for the passed in indexPath from the photoReferences array property.
+     Get the URL for the associated image using the imageURL property. Use .usingHTTPS (provided in URL+Secure.swift) to make sure the URL is an https URL. By default, the API returns http URLs.
+     Create and run a data task to load the image data from the imageURL.
+     In the data task's completion handler, check for an error, and return early if there is one. Otherwise, create a UIImage from the received data.
+     Important: Check to see if the current index path for cell is the same one you were asked to load. If not, this means that that item has scrolled off screen and the UICollectionViewCell instance has been reused for a different index path. If this happens, abort setting the image.
+     If the cell hasn't been reused, set its imageView's image to the UIImage you just created.
+     Make sure you do all UIKit API calls on the main queue.
+
+ */
+
 import UIKit
 
 class PhotosCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    /// Keys are MarsPhotoReference ids. Values are image data (also could've been UIImage)
+    let cache = Cache<Int, Data>()
+    private let photoFetchQueue = OperationQueue()
+    var operation: [Int : Operation] = [ : ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,14 +83,46 @@ class PhotosCollectionViewController: UIViewController, UICollectionViewDataSour
         return UIEdgeInsets(top: 0, left: 10.0, bottom: 0, right: 10.0)
     }
     
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        let photoReference = photoReferences[indexPath.item]
+        let key = photoReference.id
+        operation[key]?.cancel()
+    }
+    
     // MARK: - Private
     
     private func loadImage(forCell cell: ImageCollectionViewCell, forItemAt indexPath: IndexPath) {
         
-        // let photoReference = photoReferences[indexPath.item]
+        let photoReference = photoReferences[indexPath.item]
         
         // TODO: Implement image loading here
         // DONT TELL ME WHAT TO DO
+        
+        let key = photoReference.id
+        
+        if let cachedData = cache.value(for: key) {
+            let image = UIImage(data: cachedData)
+            cell.imageView.image = image
+        } else {
+            let fetchImage = FetchPhotoOperation(reference: photoReference)
+            let storeCacheData = BlockOperation {
+                if let data = fetchImage.imageData {
+                    self.cache.cache(value: data, for: key)
+                }
+            }
+                       
+            let setCellImage = BlockOperation {
+                if let imageData = fetchImage.imageData {
+                    cell.imageView.image = UIImage(data: imageData)
+                }
+            }
+            storeCacheData.addDependency(fetchImage)
+            setCellImage.addDependency(fetchImage)
+            photoFetchQueue.addOperations([fetchImage, storeCacheData], waitUntilFinished: false)
+            OperationQueue.main.addOperation(setCellImage)
+            
+            operation[key] = fetchImage
+        }
     }
     
     // Properties
