41df4a4dbda8e1c5617e18fcd2e778d17083d43a
elizabeth paige <wingateep@Icloud.com>
2020-03-12 18:34:14 -0400

    finished the func cache + func value

--- Astronomy/Cache/Cache.swift
+++ Astronomy/Cache/Cache.swift
@@ -11,5 +11,16 @@ import Foundation
 class Cache<Key,Value> where Key: Hashable {
 
     private var cachedDictionary: [Key: Value] = [:]
+    private let queue = DispatchQueue(label: "PhotoCacheQueue")
     
+    func addCache(value: Value, for key: Key) {
+        queue.async {
+            self.cachedDictionary[key] = value
+        }
+    }
+    func value(for key: Key) -> Value? {
+        queue.sync {
+            return cachedDictionary[key]
+        }
+    }
 }
