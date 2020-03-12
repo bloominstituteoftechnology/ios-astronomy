caea0736851ce28ec9769572e1d5204b2a151f68
Enrique Gongora <EGTech24@gmail.com>
2020-03-12 13:54:44 -0700

    Thread Safety implemented. No longer crashes.

--- Astronomy/Model/Cache.swift
+++ Astronomy/Model/Cache.swift
@@ -10,12 +10,17 @@ import Foundation
 
 class Cache<Key: Hashable, Value> {
     private var storage = [Key: Value]()
+    private let queue = DispatchQueue(label: "CacheQueue")
     
     func cache(value: Value, for key: Key) {
-        storage[key] = value
+        queue.sync {
+            self.storage[key] = value
+        }
     }
     
     func value(for key: Key) -> Value? {
-        return storage[key] ?? nil
+        queue.sync {
+            return storage[key] ?? nil
+        }
     }
 }
