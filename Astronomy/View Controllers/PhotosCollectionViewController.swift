f5236cd7d6b300ad229f880e09aad2f7091128ff
Lydia Zhang <art4everyan@gmail.com>
2020-04-09 17:52:49 -0400

    MVP lydia

--- Astronomy/View Controllers/PhotosCollectionViewController.swift
+++ Astronomy/View Controllers/PhotosCollectionViewController.swift
@@ -60,6 +60,12 @@ class PhotosCollectionViewController: UIViewController, UICollectionViewDataSour
         return UIEdgeInsets(top: 0, left: 10.0, bottom: 0, right: 10.0)
     }
     
+    func collectionView(_ collectionView: UICollectionView,didEndDisplaying cell: UICollectionViewCell,forItemAt indexPath: IndexPath) {
+        let reference = photoReferences[indexPath.row]
+        guard let thisOperation = operation[reference.id] else {return}
+        thisOperation.cancel()
+    }
+    
     // MARK: - Private
     
     private func loadImage(forCell cell: ImageCollectionViewCell, forItemAt indexPath: IndexPath) {
