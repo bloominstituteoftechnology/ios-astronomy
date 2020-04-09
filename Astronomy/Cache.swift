b4c4b2e1f37098aed49527dece586fb8eac8d04c
Mark Gerrior <github@gerrior.com>
2020-04-09 15:21:20 -0700

    Initial implementation of Cache

---
+++ Astronomy/Cache.swift
@@ -0,0 +1,21 @@
+//
+//  Cache.swift
+//  Astronomy
+//
+//  Created by Mark Gerrior on 4/9/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class Cache<Key: Hashable, Value> {
+    private var dict: [Key: Value] = [:]
+    
+    func cache(value: Value, for: Key) {
+        // FIXME: Implement
+    }
+
+    func value(for: Key) {
+        // FIXME: Implement
+    }
+}
