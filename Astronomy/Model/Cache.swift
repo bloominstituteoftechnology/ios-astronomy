ab3d7d924a0c0df146349111973c0d045163d1db
Hector L <hectoremil21@hotmail.com>
2020-04-09 16:14:05 -0400

    cache(value:, for:). value(for key:) returns optional Value for now

--- Astronomy/Model/Cache.swift
+++ Astronomy/Model/Cache.swift
@@ -11,4 +11,11 @@ import Foundation
 class Cache<Key: Hashable, Value> {
     private var cache: [Key : Value] = [ : ]
     
+    func cache(value: Value, for key: Key) {
+        cache[key] = value
+    }
+    
+    func value(for key: Key) -> Value? {
+        return cache[key]
+    }
 }
