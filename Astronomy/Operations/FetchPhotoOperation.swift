b08af75ce4cf17b4d28f4c86302b42d6b5886617
Shawn Gee <shawngee@gmail.com>
2020-04-09 16:26:11 -0400

    Made fetch photo operation

---
+++ Astronomy/Operations/FetchPhotoOperation.swift
@@ -0,0 +1,48 @@
+//
+//  FetchPhotoOperation.swift
+//  Astronomy
+//
+//  Created by Shawn Gee on 4/9/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class FetchPhotoOperation: ConcurrentOperation {
+    var photoReference: MarsPhotoReference
+    var imageData: Data?
+    
+    init(photoReference: MarsPhotoReference) {
+        self.photoReference = photoReference
+    }
+    
+    override func start() {
+        state = .isExecuting
+        
+        guard let imageURl = photoReference.imageURL.usingHTTPS else { return }
+        
+        fetchPhotoTask = URLSession.shared.dataTask(with: imageURl) { (data, response, error) in
+            defer { self.state = .isFinished }
+            
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
+                print("Invalid response: \(response.statusCode)")
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            self.imageData = data
+        }
+    }
+    
+    override func cancel() {
+        fetchPhotoTask?.cancel()
+    }
+    
+    private var fetchPhotoTask: URLSessionDataTask?
+}
