aa06e091bb5f100fede62ae12825df5b6a7f8819
Brad <braddiroff@icloud.com>
2020-04-09 18:10:27 -0400

    Finished FetchPhotoOperation

--- Astronomy/Operations/FetchPhotoOperation.swift
+++ Astronomy/Operations/FetchPhotoOperation.swift
@@ -7,9 +7,39 @@
 //
 
 import Foundation
+import UIKit
 
 class FetchPhotoOperation: ConcurrentOperation {
     
+    var reference: MarsPhotoReference
+    var imageData: Data?
+    private(set) var session = URLSession(configuration: .default)
     
+    init(reference: MarsPhotoReference) {
+        self.reference = reference
+        
+    }
+    
+    func runDataTask() {
+        session.dataTask(with: reference.imageURL.usingHTTPS!) { (data, _, error) in
+             if let error = error {
+                 print("Error downloading picture: \(error)")
+                 return
+             } else {
+                if let data = data {
+                    self.imageData = data
+                    self.state = .isFinished
+            }
+            }
+        }
+    }
+    
+    override func start() {
+        state = .isExecuting
+    }
+    
+    override func cancel() {
+        session.invalidateAndCancel()
+    }
     
 }
