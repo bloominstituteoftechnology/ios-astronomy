f85269a3e688575596785dea936392a1c869e09a
Brad <braddiroff@icloud.com>
2020-04-09 17:27:11 -0400

    Started creating FetchPhotoOperation

---
+++ Astronomy/Operations/FetchPhotoOperation.swift
@@ -0,0 +1,15 @@
+//
+//  FetchPhotoOperation.swift
+//  Astronomy
+//
+//  Created by Bradley Diroff on 4/9/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class FetchPhotoOperation: ConcurrentOperation {
+    
+    
+    
+}
