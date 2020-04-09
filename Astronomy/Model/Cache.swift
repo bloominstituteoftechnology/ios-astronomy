d0311b04830ab16889a4c35e24d912ba7f8526c8
Hector L <hectoremil21@hotmail.com>
2020-04-09 16:06:47 -0400

    Create Cache.swift. class Cache<Key: Hashable, Value>. private dictionary cache.

---
+++ Astronomy/Model/Cache.swift
@@ -0,0 +1,14 @@
+//
+//  Cache.swift
+//  Astronomy
+//
+//  Created by Karen Rodriguez on 4/9/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class Cache<Key: Hashable, Value> {
+    private var cache: [Key : Value] = [ : ]
+    
+}
