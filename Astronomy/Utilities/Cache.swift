40417f0d4fd10c9dc3618d65ada4a8f1c1858b46
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-07 16:01:49 -0700

    Added cache and implemented class cache

---
+++ Astronomy/Utilities/Cache.swift
@@ -0,0 +1,14 @@
+//
+//  Cache.swift
+//  Astronomy
+//
+//  Created by Bhawnish Kumar on 5/7/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class Cache<Key: Hashable, Value> {
+    private var dict: [Key: Value] = [:]
+    
+}
