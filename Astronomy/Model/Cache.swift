35049e51deeefe1a7841c4c4bc8470b430db5eaa
Mark Poggi <markp@mac.com>
2020-05-07 13:05:42 -0700

    Set up the add to cache and return from cache methods in Cache class.  Added queue property from further ahead as a placeholder for now.

--- Astronomy/Model/Cache.swift
+++ Astronomy/Model/Cache.swift
@@ -10,8 +10,27 @@ import Foundation
 
 class Cache<Key: Hashable, Value> {
 
+    // set up the cache
     private var cache = [Key : Value]()
 
+    // created a serial queue
+    private var queue = DispatchQueue(label: "placeholder")
 
-    
+    // add items to the cache of type dictionary
+    func cache(value: Value, key: Key) {
+        queue.async {
+            self.cache[key] = value
+        }
+    }
+
+    // return items from the cache of type dictionary
+    func value(key: Key) -> Value? {
+        // Making this sync rather than async.  Needs to work with other threads.
+        return queue.sync {
+            cache[key]
+        }
+    }
+
+
+
 }
