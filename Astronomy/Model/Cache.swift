6f9afcc1ff55909188d19f16d2588b1466993549
Harmony Radley <harmonyradley99@gmail.com>
2020-06-04 16:43:37 -0400

    Created Cache file  and constrained to conform to Hashable. Also Created a private property

---
+++ Astronomy/Model/Cache.swift
@@ -0,0 +1,16 @@
+//
+//  Cache.swift
+//  Astronomy
+//
+//  Created by Harmony Radley on 6/4/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class Cache<Key: Hashable, Value> {
+
+    private var cache = [Key : Value]()
+    
+
+}
