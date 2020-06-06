83aa977f97fb39699a08bf9728cc7e5294e988aa
Stephanie Ballard <user.name>
2020-06-06 15:18:48 -0700

    saving

--- Astronomy/Model/Cache.swift
+++ Astronomy/Model/Cache.swift
@@ -10,15 +10,20 @@ import UIKit
 
 class Cache<Key: Hashable, Value> {
     
+    // a place for items to be cached
     private var cache = [Key : Value]()
+    
+    // serial queue so that everyone can use shared resources without using nslock
     private let queue = DispatchQueue(label: "com.LambdaSchool.Astronomy.CacheQueue")
     
+    // have a function to add items to the cache
     func cache(value: Value, for key: Key) {
         queue.async {
             self.cache[key] = value
         }
     }
     
+    // have a function to return items that are cache, optional in case it doesn't exist
     func value(for key: Key) -> Value? {
         return queue.sync {
             cache[key]
