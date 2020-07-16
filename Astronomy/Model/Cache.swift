87c0a11119109ad671918c35b5c27b86c4b55cd2
Clayton Watkins <watkinclayton17@yahoo.com>
2020-07-16 13:31:26 -0700

    Implemented a private queue for the dictionary; Completed Part 3

--- Astronomy/Model/Cache.swift
+++ Astronomy/Model/Cache.swift
@@ -11,14 +11,19 @@ import Foundation
 class Cache<Key: Hashable, Value>{
     
     private var storedCache:[Key : Value] = [:]
+    private let queue = DispatchQueue(label: "private queue for our Cache")
     
     func storeInCache(value: Value, for key: Key){
-        self.storedCache[key] = value
+        queue.async {
+            self.storedCache[key] = value
+        }
     }
     
     func getValue(for key: Key) -> Value?{
-        guard let storedValue = self.storedCache[key] else { return nil}
-        return storedValue
+        queue.sync {
+            guard let storedValue = self.storedCache[key] else { return nil}
+            return storedValue
+        }
     }
     
 }
